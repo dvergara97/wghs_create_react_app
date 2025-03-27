4af4c9a17aa0521f1d5040ff166d41ac893c12b3
daniel.vergara <daniel.vergara@sahuarita.net>
2025-03-27 15:00:23 -0700

    Updating to not use deprecated code

--- our_app/src/index.js
+++ our_app/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+
+import { createRoot } from 'react-dom/client';
+
+
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +15,14 @@ const App = () => {
     );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
