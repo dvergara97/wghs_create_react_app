2b9a035cba8d3ae4ce686964547d916e4050ffa8
daniel.vergara <daniel.vergara@sahuarita.net>
2025-03-27 15:20:18 -0700

    Step 2

--- our_app/src/index.js
+++ our_app/src/index.js
@@ -7,43 +7,34 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const Panel = ({ title, children }) => {
-   const [isActive, setIsActive] = useState(false);
-   return (
-     <section className="panel">
-       <h3>{title}</h3>
-       {isActive ? (
-         <p>{children}</p>
-       ) : (
-         <button onClick={() => setIsActive(true)}>
-           Show
-         </button>
-       )}
-     </section>
-   );
- }
+const Panel = ({ title, children, isActive }) => {
+  return (
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? (
+        <p>{children}</p>
+      ) : (
+        <button onClick={() => setIsActive(true)}>
+          Show
+        </button>
+      )}
+    </section>
+  );
+}
 
- const Accordion = () => {
-   return (
-     <>
-       <h2>Almaty, Kazakhstan</h2>
-       <Panel title="About">
-         With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-       </Panel>
-       <Panel title="Etymology">
-         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-       </Panel>
-     </>
-   );
- }
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <Accordion />
-  </React.StrictMode>
-);
+const Accordion = () => {
+  return (
+    <>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel title="About" isActive={true}>
+        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel title="Etymology" isActive={true}>
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+      </Panel>
+    </>
+  );
+}
 
 
 // If you want to start measuring performance in your app, pass a function
